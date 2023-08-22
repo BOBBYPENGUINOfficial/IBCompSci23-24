da6f864c232f56ad48d63c6e23e7c7d079715992
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2023-08-22 08:51:33 -0600

    Add files via upload

---
+++ PaintingTester/src/PaintingTester.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PaintingTester{
+    //Creates a Piet Mondrian painting
+    public static void main(String []args){
+        Random rand = new Random();
+        System.out.println("|@@@@@@@|:::::::|!!!!!!!|");
+        System.out.println("|@@@@@@@|:::::::|!!!!!!!|");
+        System.out.println("|@@@@@@@|:::::::|!!!!!!!|");
+        System.out.println("|@@@@@@@|:::::::|!!!!!!!|");
+        System.out.println("|&&&&&&&&&&&|BBBBBBBBBBB|");
+        System.out.println("|&&&&&&&&&&&|BBBBBBBBBBB|");
+        System.out.println("|&&&&&&&&&&&|BBBBBBBBBBB|");
+        System.out.println("|&&&&&&&&&&&|BBBBBBBBBBB|");
+        System.out.println("|YYYYYYY|***************|");
+        System.out.println("|YYYYYYY|***************|");
+
+    }
+}
